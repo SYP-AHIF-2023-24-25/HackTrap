eeb7f981a0b61fab810addb5efca1811e3ff3a2e
Christian Ekhator <christian.ekhator06@gmail.com>
2023-12-03 23:35:08 +0100

    ipad feature combined with usertracking

---
+++ Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Set the initial color
+        SetRandomColor();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check for mouse click
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Change the color when clicked
+            SetRandomColor();
+        }
+    }
+
+    // Function to set a random color
+    void SetRandomColor()
+    {
+        // Generate random color values
+        float r = Random.value;
+        float g = Random.value;
+        float b = Random.value;
+
+        // Set the object's color
+        GetComponent<Renderer>().material.color = new Color(r, g, b);
+    }
+}
---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public string nextSceneName;
+
+    private void OnMouseDown()
+    {
+        // Check if the next scene name is not null or empty
+        if (!string.IsNullOrEmpty(nextSceneName))
+        {
+            // Load the next scene
+            SceneManager.LoadScene(nextSceneName);
+        }
+        else
+        {
+            Debug.LogError("Next scene name is not set. Please assign a scene name in the inspector.");
+        }
+    }
+}
