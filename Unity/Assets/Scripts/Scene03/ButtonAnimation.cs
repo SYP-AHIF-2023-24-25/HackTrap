896cc75417f695a0978c52b8331cc7fe32de5939
Julian-Kapl <j.kapl@students.htl-leonding.ac.at>
2024-01-06 18:04:01 +0100

    ButtonAnimation

---
+++ Unity/Assets/Scripts/Scene03/ButtonAnimation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonAnimation : MonoBehaviour
+{
+    public Sprite sprite;
+    public Button btn;
+    private void Start()
+    {
+        btn.onClick.AddListener(delegate { ChangeImage(); });
+    }
+    public void SetText(string text)
+    {
+        Text txt = transform.Find("Text").GetComponent<Text>();
+        txt.text = text;
+    }
+    public void ChangeImage()
+    {
+        btn.GetComponent<Image>().sprite = sprite;
+        btn.GetComponent<Image>().color = Color.green;
+    }
+}
