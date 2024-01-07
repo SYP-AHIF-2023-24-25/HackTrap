093f7f82a20651ccc1b6b07c10a0f9100914873b
Amina <amina27gabeljic@gmail.com>
2024-01-07 12:48:20 +0100

    Hacking + KnockOut + Sound done

---
+++ Unity/Assets/PopupOne.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+using UnityEngine;
+
+public class PopupOne : MonoBehaviour
+{
+    public int time;
+    public int positionX;
+    public int positionY;
+    private Canvas canvas;
+
+    private float sceneRuntime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sceneRuntime = Time.time;
+        canvas = GetComponent<Canvas>();
+        canvas.enabled = false;
+
+        Invoke("showCanvas", time);
+    }
+
+    void showCanvas()
+    {
+        canvas.enabled = true;
+        GetComponent<Canvas>().GetComponent<RectTransform>().anchoredPosition = new Vector2(positionX, positionY);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sceneRuntime = Time.time - sceneRuntime;
+
+        UnityEngine.Debug.Log("Runtime: " + sceneRuntime);
+
+        if(sceneRuntime > 10.4f ) {
+            canvas.enabled = false;
+        }
+    }
+}
---
+++ Unity/Assets/SceneChanger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneChanger : MonoBehaviour
+{
+    void Start()
+    {
+        Invoke("changeScene", 5f);
+    }
+
+    void changeScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
