6620d110aa26da3ad13a814c659a0b3efcc35648
julia-meyr <julia.meyr06@gmail.com>
2024-01-07 15:29:04 +0100

    added dynamic columns for more players

--- Unity/Assets/Scripts/JuliasNewScene/PlayerManager.cs
+++ Unity/Assets/Scripts/JuliasNewScene/PlayerManager.cs
@@ -4,25 +4,37 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
+    //public UserTracker userTracker;
     public GameObject playerPrefab;
-    public int columns = 4; 
+    public int minColumns = 4;
+    public int numberOfPlayers = 20; //irgendeine zahl
+
+    private bool[] players;
 
     void Start()
     {
+        //numberOfPlayers = userTracker.GetPlayerCount(); //--> später: number of players vom usertracking
+        players = new bool[numberOfPlayers];
         InstantiatePlayers();
     }
 
+    /// <summary>
+    /// zeichnet die felder, um die spieler
+    /// als player für teams zu inizialisieren
+    /// </summary>
     void InstantiatePlayers()
     {
-        int numberOfPlayers = 10; // userTracker.GetPlayerCount(); //--> später: number of players vom usertracking
-
         // X spacing ist in Unity immer 800
         // Y spacing ist in Unity immer 600
 
         float spacingX = 2.0f; 
-        float spacingZ = 2.0f; 
+        float spacingZ = 2.0f;
 
-        int rows = Mathf.CeilToInt((float)numberOfPlayers / columns);
+        int maxColumns = Mathf.Max(minColumns, Mathf.CeilToInt(Mathf.Sqrt(numberOfPlayers))); // Spaltenanzahl berechnen, wenn z.B. mehr spieler als 4^2 sind, werden spalten mehr
+
+        int columns = Mathf.Clamp(maxColumns, minColumns, int.MaxValue); 
+        int rows = Mathf.CeilToInt((float)numberOfPlayers / columns); // Anzahl der Reihen berechnen
+
 
         for (int row = 0; row < rows; row++)
         {
@@ -40,5 +52,25 @@ public class PlayerManager : MonoBehaviour
                 }
             }
         }
+    }
+
+    void Update()
+    {
+
+    }
+
+    /// <summary>
+    /// überprüft, ob alle spieler in einem Feld sind
+    /// </summary>
+    bool AllFieldsOccupied()
+    {
+        for (int i = 0; i < players.Length; i++)
+        {
+            if (!players[i])
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
