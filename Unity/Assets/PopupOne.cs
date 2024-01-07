c7af730413640342e2208046aa1d0987fec0d9d4
Amina <amina27gabeljic@gmail.com>
2024-01-07 23:21:17 +0100

    updated game

--- Unity/Assets/PopupOne.cs
+++ Unity/Assets/PopupOne.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Threading;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PopupOne : MonoBehaviour
 {
@@ -13,18 +14,30 @@ public class PopupOne : MonoBehaviour
     private Canvas canvas;
 
     private float sceneRuntime;
+
     // Start is called before the first frame update
     void Start()
     {
-        sceneRuntime = Time.time;
+        SceneManager.activeSceneChanged += ChangedActiveScene;
+
+        UnityEngine.Debug.Log("Runtime debug: Start");
+
+        sceneRuntime = Time.timeSinceLevelLoad;
         canvas = GetComponent<Canvas>();
         canvas.enabled = false;
 
         Invoke("showCanvas", time);
     }
 
+    private void ChangedActiveScene(Scene current, Scene next)
+    {
+        UnityEngine.Debug.Log("Runtime debug: Change of Scene");
+        sceneRuntime = Time.time;
+    }
+
     void showCanvas()
     {
+
         canvas.enabled = true;
         GetComponent<Canvas>().GetComponent<RectTransform>().anchoredPosition = new Vector2(positionX, positionY);
     }
@@ -32,11 +45,11 @@ public class PopupOne : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        sceneRuntime = Time.time - sceneRuntime;
+        sceneRuntime = Time.timeSinceLevelLoad - sceneRuntime;
 
-        UnityEngine.Debug.Log("Runtime: " + sceneRuntime);
+        UnityEngine.Debug.Log("Runtime debug: " + sceneRuntime);
 
-        if(sceneRuntime > 11f ) {
+        if(sceneRuntime > 10.2f ) {
             canvas.enabled = false;
         }
     }
