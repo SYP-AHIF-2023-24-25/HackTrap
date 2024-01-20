6021eb9a7d25ee741c561f3701386866455dc9c7
Christian Ekhator <christian.ekhator06@gmail.com>
2024-01-20 01:12:25 +0100

    Usertracking with scene switch added

---
+++ Unity/Assets/Scripts/Scene00/Collision.cs
@@ -0,0 +1,38 @@
+﻿using DeepSpace.LaserTracking;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Collision : MonoBehaviour
+{
+    private bool _collided = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private IEnumerator OnTriggerEnter(Collider other)
+    {
+        _collided = true;
+
+        yield return new WaitForSeconds(3); // Wait for 3 seconds to check, if someone is still standing on the button.
+        if (_collided == true) // check, if someone is still on the button
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        _collided = false;
+    }
+}
