8d0027b1edbad7f3d77668f854e26bda32e237b3
julia-meyr <julia.meyr06@gmail.com>
2024-02-27 20:00:13 +0100

    container animation

---
+++ Unity/Assets/Scripts/ContainerAnimation/MoveContainer.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoveContainer : MonoBehaviour
+{
+    [SerializeField] Transform[] positions;
+
+    private int nextPositionIndex;
+    private Transform nextPosition;
+    private bool isMoving = true;
+
+    void Start()
+    {
+        nextPosition = positions[0];
+    }
+
+    void Update()
+    {
+        if (isMoving)
+        {
+            MoveContainerToNextPosition();
+        }
+        else
+        {
+            StartBigContainerAnimation();
+        }
+    }
+
+    void MoveContainerToNextPosition()
+    {
+        if(Vector3.Distance(transform.position, nextPosition.position) < 0.1f)
+        {
+            nextPositionIndex++;
+            if(nextPositionIndex >= positions.Length)
+            {
+                isMoving = false;
+                Destroy(gameObject);
+                return;
+            }
+            nextPosition = positions[nextPositionIndex];
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, nextPosition.position, 1 * Time.deltaTime);
+        }
+    }
+
+    void StartBigContainerAnimation()
+    {
+
+    }
+}
