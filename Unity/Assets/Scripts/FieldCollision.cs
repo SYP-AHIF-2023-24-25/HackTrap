610e45a394a751eebc7d3a342d67c48ab40cc1d7
Christian Ekhator <christian.ekhator06@gmail.com>
2024-02-27 23:20:18 +0100

    Scenes Updated; Folder Structure improved

---
+++ Unity/Assets/Scripts/FieldCollision.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class FieldCollision : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+}
