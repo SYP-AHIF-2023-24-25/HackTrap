736cd9509558229cdd7d7323d85d6588c0967a6a
julia-meyr <julia.meyr06@gmail.com>
2024-04-02 22:18:31 +0200

    welcome scene updated

---
+++ Unity/Assets/Scripts/All/OnCollisionKopie.cs
@@ -0,0 +1,18 @@
+﻿using DeepSpace.LaserTracking;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class OnCollisionKopie : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collision - Skip to next scene");
+
+        // Do something if tracker collides with this object
+        StateManager.Instance.SwitchToNextScenePrefab();
+
+    }
+}
